8d7fb86ab3643513e4c5032217aa3db65965da23
Mountains-and-rivers <156110386@qq.com>
2019-02-27 21:28:18 +0800

    win10环境

---
+++ 二，demo/py/打印中文.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+ 
+import io
+import sys
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
+
+print(sys.getfilesystemencoding())
+
+s = "中文"  #py3 默认就是utf8 unicode向下兼容utf-8
+#s_to_gpk = s.encode("gbk")
+#print(s_to_gpk.decode("gbk")) #py3是不需要转换的，但是要转的话还要转回去
+print("后公钥多少啊dasd")
