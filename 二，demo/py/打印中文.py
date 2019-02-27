8557d3da6d41c24c0fc6612854c360e3d5826de8
Legendary <156110386@qq.com>
2019-02-27 21:45:44 +0800

    Update 打印中文.py

--- 二，demo/py/打印中文.py
+++ 二，demo/py/打印中文.py
@@ -9,4 +9,4 @@ print(sys.getfilesystemencoding())
 s = "中文"  #py3 默认就是utf8 unicode向下兼容utf-8
 #s_to_gpk = s.encode("gbk")
 #print(s_to_gpk.decode("gbk")) #py3是不需要转换的，但是要转的话还要转回去
-print("后公钥多少啊dasd")
+print("语言学习")
