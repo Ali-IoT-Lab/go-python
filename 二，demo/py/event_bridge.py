85fc1b6b6159dd7e87eb60a36722fab9bbf128b9
王贵林 <wangguilin@wangguilins-MacBook-Air.local>
2019-02-28 19:19:51 +0800

    组件修改

--- 二，demo/py/event_bridge.py
+++ 二，demo/py/event_bridge.py
@@ -7,19 +7,21 @@ payload = {'some': 'data'}
 headers = {'content-type': 'application/json'}
 
 auth = {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NTEzMjMxNTUsImV4cCI6MTg2NjY4MzE1NSwiaXNzIjoiN2EyNjI2YzE0ZWYwNDgzZjk4ZmM4MDkzNDExMjFkODIifQ.zJ8a9W9rkYoIjQPM822tVB8aeGZ39FviBL8Iz7Mxys4'}
-messages = SSEClient('http://192.168.31.95:8123/api/stream', headers=auth)
 
-for msg in messages:
-     outputMsg = msg.data
-     #print(outputMsg)
-     if outputMsg != 'ping':
-        print('---------------------')
-        outputJS = json.loads(outputMsg)
-        #print( FilterName, outputJS[FilterName] )
-        print(outputJS['data'])
-        ret = requests.post(url, data=json.dumps(outputJS['data']), headers=headers)
-        print(ret)
-   
+def setup(hass, config):
+    messages = SSEClient('http://192.168.31.95:8123/api/stream', headers=auth)
+
+    for msg in messages:
+       outputMsg = msg.data
+       #print(outputMsg)
+       if outputMsg != 'ping':
+          print('---------------------')
+          outputJS = json.loads(outputMsg)
+          #print( FilterName, outputJS[FilterName] )
+          print(outputJS['data'])
+          ret = requests.post(url, data=json.dumps(outputJS['data']), headers=headers)
+          print(ret)
+      
 
 
 
