71ae58251c4382d8bceb1d56edccd9d80f440ece
Legendary <156110386@qq.com>
2019-02-28 22:39:18 +0800

    Update event_bridge.py

--- 二，demo/py/event_bridge.py
+++ 二，demo/py/event_bridge.py
@@ -8,7 +8,7 @@ headers = {'content-type': 'application/json'}
 
 auth = {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NTEzMjMxNTUsImV4cCI6MTg2NjY4MzE1NSwiaXNzIjoiN2EyNjI2YzE0ZWYwNDgzZjk4ZmM4MDkzNDExMjFkODIifQ.zJ8a9W9rkYoIjQPM822tVB8aeGZ39FviBL8Iz7Mxys4'}
 
-def setup(hass, config):
+def setup():
     messages = SSEClient('http://192.168.31.95:8123/api/stream', headers=auth)
 
     for msg in messages:
