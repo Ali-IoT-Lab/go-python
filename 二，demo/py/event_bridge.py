b3e33939cf731486f105cea82a9d3433eaada8dd
Mountains-and-rivers <156110386@qq.com>
2019-03-03 21:17:47 +0800

    组件加载测试

--- 二，demo/py/event_bridge.py
+++ 二，demo/py/event_bridge.py
@@ -1,31 +1,17 @@
+from sseclient import SSEClient
 import requests
 import json
-from sseclient import SSEClient
-
-url = 'http://47.97.210.118/push_event'
+url = 'http://192.168.1.5:8123/api/stream'
+server_url = 'http://47.97.210.118/push_event'
 payload = {'some': 'data'}
 headers = {'content-type': 'application/json'}
 
-auth = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NTE0MjA0NzIsImV4cCI6MTg2Njc4MDQ3MiwiaXNzIjoiMDhiYzQ0YzYzMDQ4NGMyOTg4MzIzMWFkZjRkMmY2ZTMifQ.A6cbNYwsqqXFFrG83jebJ1LzQ8VZBs8JiytoLolZb70'}
-messages = SSEClient('http://192.168.31.94:8123/api/stream', headers=auth)
-def setup(hass, config):
-    a = hass
-    b = config
+auth = {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI5YjRjNDI5Yzg2MmY0OGFjOWJmODk0NjdjYjRhNjI0OSIsImV4cCI6MTg2Njk3NzkyNywiaWF0IjoxNTUxNjE3OTI3fQ.lWNLKS18WCvsP_SocEA7lf1-ZyjVxEbU3slvZtZhNtI'}
+
+def setup(hass, config=None):
+    messages = SSEClient(url, headers=auth)
     for msg in messages:
-       outputMsg = msg.data
-       #print(outputMsg)
-       if outputMsg != 'ping':
-          print('---------------------')
-          outputJS = json.loads(outputMsg)
-          #print( FilterName, outputJS[FilterName] )
-          print(outputJS['data'])
-          ret = requests.post(url, data=json.dumps(outputJS['data']), headers=headers)
-          print(ret)
-      
-
-#if __name__ == '__main__':
-#    setup('1','2')
-
-
-
-
+      outputMsg = msg.data
+      if outputMsg != 'ping':
+        ret_server = requests.post(url, data=json.dumps(payload), headers=headers)
+    return True 
