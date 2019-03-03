e7a94c54fc4df702c150b0ec2715fe00fc7f0d8c
Mountains-and-rivers <156110386@qq.com>
2019-03-03 21:39:09 +0800

    组件测试

--- 二，demo/py/event_bridge.py
+++ 二，demo/py/event_bridge.py
@@ -7,11 +7,20 @@ payload = {'some': 'data'}
 headers = {'content-type': 'application/json'}
 
 auth = {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI5YjRjNDI5Yzg2MmY0OGFjOWJmODk0NjdjYjRhNjI0OSIsImV4cCI6MTg2Njk3NzkyNywiaWF0IjoxNTUxNjE3OTI3fQ.lWNLKS18WCvsP_SocEA7lf1-ZyjVxEbU3slvZtZhNtI'}
+messages = SSEClient(url, headers=auth)
+
 
 def setup(hass, config=None):
-    messages = SSEClient(url, headers=auth)
-    for msg in messages:
-      outputMsg = msg.data
-      if outputMsg != 'ping':
-        ret_server = requests.post(url, data=json.dumps(payload), headers=headers)
-    return True 
+    # for msg in messages:
+     # outputMsg = msg.data
+      # if outputMsg != 'ping':
+
+    print('111111111111111111111111111111111111111')
+
+    print(payload)
+    ret_server = requests.post(url, data=json.dumps(payload), headers=headers)
+    print(ret_server)
+    return True
+
+#if __name__ == '__main__':
+#    setup('1', '2')
