a18444318a72efb31443e8d1921f0cd9a478462b
Mountains-and-rivers <156110386@qq.com>
2019-03-04 01:13:34 +0800

    监听一处

--- 二，demo/py/event_bridge_com.py
+++ 二，demo/py/event_bridge_com.py
@@ -1,11 +1,41 @@
 DOMAIN = 'event_bridge'
+from datetime import timedelta
+import functools as ft
+
+from homeassistant.loader import bind_hass
+from homeassistant.helpers.sun import get_astral_event_next
+from ..core import HomeAssistant, callback
+from ..util.async_ import run_callback_threadsafe
 import requests
 import json
 
 server_url = 'http://47.97.210.118/push_event'
 headers = {'content-type': 'application/json'}
 
-def setup(hass, config):
+def threaded_listener_factory(async_factory):
+    """Convert an async event helper to a threaded one."""
+    @ft.wraps(async_factory)
+    def factory(*args, **kwargs):
+        """Call async event helper safely."""
+        hass = args[0]
+
+        if not isinstance(hass, HomeAssistant):
+            raise TypeError('First parameter needs to be a hass instance')
+
+        async_remove = run_callback_threadsafe(
+            hass.loop, ft.partial(async_factory, *args, **kwargs)).result()
+
+        def remove():
+            """Threadsafe removal."""
+            run_callback_threadsafe(hass.loop, async_remove).result()
+
+        return remove
+
+    return factory
+
+@callback
+@bind_hass
+def async_track_state_change(hass, config):
     """Set up is called when Home Assistant is loading our component."""
     count = 0
 
@@ -19,3 +49,4 @@ def setup(hass, config):
 
     # Listen for when my_cool_event is fired
     return hass.bus.async_listen_once('state_changed', handle_event)
+track_state_change = threaded_listener_factory(async_track_state_change)
