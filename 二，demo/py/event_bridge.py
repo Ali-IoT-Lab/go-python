e102e8cf89d37fb50870f49bd983ea852d4c8900
Mountains-and-rivers <156110386@qq.com>
2019-03-04 02:57:14 +0800

    1

--- 二，demo/py/event_bridge.py
+++ 二，demo/py/event_bridge.py
@@ -1,20 +1,21 @@
 DOMAIN = 'event_bridge'
+
 import requests
 import json
 
-server_url = 'http://47.97.210.118/push_event'
+#server_url = 'http://47.97.210.118/push_event'
 headers = {'content-type': 'application/json'}
+
 def setup(hass, config):
     """Set up is called when Home Assistant is loading our component."""
     count = 0
-
+    a=config
     # Listener to handle fired events
     def handle_event(event):
-        nonlocal count
-        count += 1
-        print('Total events received:', count)
+        print(event)
         new_state = event.data.get('new_state')
-        requests.post(server_url, data=json.dumps({'some': 'data'}), headers=headers)
+        requests.post(config.resource, data=json.dumps({'some': 'data'}), headers=headers)
 
     # Listen for when state_changed is fired
-    hass.bus.listen('state_changed', handle_event)
+    hass.bus.listen('s11tate_changed', handle_event)
+          return True
