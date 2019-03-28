0558ab104922e5dd938217a9b8aa8f944e92e606
Mountains-and-rivers <sjdykp@163.com>
2019-03-28 15:39:14 +0800

    文件流读取执行结果

--- 二，demo/golang/pty命令执行测试/client.go
+++ 二，demo/golang/pty命令执行测试/client.go
@@ -1,30 +1,38 @@
-
-
-
-
-
 package main
 
 import (
-	"github.com/kr/pty"
-	"io"
+	"fmt"
 	"os"
 	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/kr/pty"
 )
 
 func main() {
-	c :=  exec.Command("/bin/bash", "-l")
+	c := exec.Command("/bin/bash", "-l")
 	f, err := pty.Start(c)
+	c.Env = os.Environ()
+	c.Env = append(c.Env, "LANG=en_US.UTF-8", "LC_ALL=en_US.UTF-8")
 	if err != nil {
 		panic(err)
 	}
 
 	go func() {
 		f.Write([]byte("l"))
-		
 		f.Write([]byte("s"))
 		f.Write([]byte("\r"))
-                f.Write([]byte{4}) // EOT
+		//f.Write([]byte{4}) // EOT
+
 	}()
-	io.Copy(os.Stdout, f)
+
+	time.Sleep(2 * time.Second)
+	//var file *os.File
+	//io.Copy(file, f)
+	buf := make([]byte, 1024)
+	f.Read(buf)
+	var strf = string(buf)
+	var a = strings.Split(strf, "\n")
+	fmt.Println(a[2])
 }
